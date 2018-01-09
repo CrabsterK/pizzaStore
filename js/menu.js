a395ad69b08173ed3adce0d9ef6b01cb9f02bd0a
CrabsterK <arek.marcinowski18@gmail.com>
2018-01-09 20:36:55 +0100

    update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,7 @@
  var menu= "" +
         "<ul>" +
-            "<li><a href=\"/PIZZA/menu.html\">Menu</a></li>" +
-            "<li><a href=\"/PIZZA/promocje.html\">Promocje</a></li>" +
+            "<li><a href=\"/PIZZA/menu.php\">Menu</a></li>" +
+            "<li><a href=\"/PIZZA/promocje.php\">Promocje</a></li>" +
             "<li><a href=\"/PIZZA/zestawDnia.html\">Zestaw dnia</a></li>" +
         "</ul>";
 
