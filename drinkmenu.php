ecd4410c4d891dcaa2533b82c410621eb611a670
CrabsterK <arek.marcinowski18@gmail.com>
2018-01-09 21:04:37 +0100

    update menus to table-menus

---
+++ drinkmenu.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="pl">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="keywords" content="pizza, salad, food, jedzenia, restauracja, nocna">    <!--TODO: keywords-->
+        <meta name="description" content="Restauracja wysyłkowa z napojami, sałatkami, pizzą.">    <!--TODO: description-->
+        <meta name="author" content="Arkadiusz Marcinowski, CrabsterK">
+        <title>Napoje</title>    <!--TODO: title-->
+        <link href="css/general.css" rel="stylesheet" type="text/css">
+    </head>
+    
+    <body>
+
+        <a href ="index.php">
+            <img id="logo" alt="logo" src="img/pizza.png">
+        </a>
+        <a href ="track.html">
+            <img id="track" alt="logo" src="img/track.png">
+        </a>
+        <a href ="user.html">
+            <img id="user" alt="logo" src="img/user.png">
+        </a>
+        <a href ="search.html">
+            <img id="search" alt="logo" src="img/search.png">
+        </a>
+
+            
+        <header id="menu">
+            <script src="js/menu.js"></script>
+        </header>
+        
+
+        <section>
+            <h2>Menu Napoje</h2>
+            <article style="height: 700px; ">
+                <br><br><br><br><br>
+
+
+                   <?php
+                        echo 
+                        "<table style=\"width:100%; text-align: center;\" >
+                             <tr>
+                                <td>  
+                                    <a href =\"product.html\">
+                                        <img id=\"polecanePic\" src=\"img/cola.jpg\">
+                                    </a>
+                                    <h5>PIZZA MIĘSNA</h5>
+                                </td>
+
+                                <td>  
+                                    <a href =\"product.html\">
+                                        <img id=\"polecanePic\" src=\"img/orange.jpg\">
+                                    </a>
+                                    <h5>PIZZA CAPRICIOSA</h5>
+                                </td>
+
+                                <td>  
+                                    <a href =\"product.html\">
+                                       <img id=\"polecanePic\" src=\"img/sprite.jpg\">
+                                    </a>
+                                    <h5>PIZZA PEPERONI</h5>
+                                </td>
+                            </tr>
+
+
+
+                             <tr>
+                                <td>  
+                                    <a href =\"product.html\">
+                                        <img id=\"polecanePic\" src=\"img/piwo.jpg\">
+                                    </a>
+                                    <h5>PIZZA RIMINI</h5>
+                                </td>
+
+                                <td>  
+                                    <a href =\"product.html\">
+                                        <img id=\"polecanePic\" src=\"img/woda.jpg\">
+                                    </a>
+                                    <h5>PIZZA BARBADOS</h5>
+                                </td>
+
+                                <td>  
+                                    <a href =\"product.html\">
+                                       <img id=\"polecanePic\" src=\"img/herbata.png\">
+                                    </a>
+                                    <h5>PIZZA TEKSAŃSKI KURCZAK</h5>
+                                </td>
+                            </tr>";
+
+
+
+
+                    ?>            
+            </article>
+
+        </section>
+
+    </body>
+    
+</html>
---
+++ saladmenu.php
@@ -0,0 +1,89 @@
+<?PHP
+    require 'DB/connection.php';
+
+
+
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="pl">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="keywords" content="pizza, salad, food, jedzenia, restauracja, nocna">    <!--TODO: keywords-->
+        <meta name="description" content="Restauracja wysyłkowa z napojami, sałatkami, pizzą.">    <!--TODO: description-->
+        <meta name="author" content="Arkadiusz Marcinowski, CrabsterK">
+        <title>Sałatki</title>    <!--TODO: title-->
+        <link href="css/general.css" rel="stylesheet" type="text/css">
+    </head>
+    
+    <body>
+
+        <a href ="index.php">
+            <img id="logo" alt="logo" src="img/pizza.png">
+        </a>
+        <a href ="track.html">
+            <img id="track" alt="logo" src="img/track.png">
+        </a>
+        <a href ="user.html">
+            <img id="user" alt="logo" src="img/user.png">
+        </a>
+        <a href ="search.html">
+            <img id="search" alt="logo" src="img/search.png">
+        </a>
+
+            
+        <header id="menu">
+            <script src="js/menu.js"></script>
+        </header>
+        
+
+        <section>
+    
+            <h2>Menu Sałatki</h2>
+            
+            <article style="height: 900px; ">
+              <br><br><br>
+
+
+                <?php
+
+                    $sql = "SELECT Nazwa, skladTowaru, Cena FROM towara WHERE Kategoria = 'Salatka'";
+                    $result = mysqli_query($link, $sql);
+                    $licz =0;
+                    if(mysqli_num_rows($result) > 0){
+                        echo "<table style=\"width:100%; text-align: left;\" >
+                           <tr>
+							    <td style=\"border-bottom: 1px solid black; width:200px\"></td>
+							    <td style=\"border-bottom: 1px solid black\">Skład sałatki</td>
+							    <td style=\"border-bottom: 1px solid black; width:120px\">Cena</td> 
+						  	</tr>";
+                        while($row = mysqli_fetch_assoc($result)){
+                        	if($licz == 2){
+                        		echo"<h4>Z MIĘSEM</h4>";  
+                        	}
+                        	$licz++;;
+                            echo "<tr >
+                            		<td style=\"border-bottom: 1px solid black; width:200px\">
+                            			<a href =\"product.html\">
+                                       		".$row["Nazwa"]."
+                                   		</a>
+                            		</td>
+								    <td style=\"border-bottom: 1px solid black;\"><i>" . $row["skladTowaru"] . "</i></td>
+								    <td style=\"border-bottom: 1px solid black; width:120px\">" . $row["Cena"]  . "zł</td> 
+								</tr>";
+                        }
+                    }
+                    echo "</table>";
+                ?> 
+
+
+
+            </article>
+
+    </body>
+    
+</html>
